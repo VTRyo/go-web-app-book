82e819e2cbdb078e07b9a6375708f9f0a59e62e2
VTRyo <vtryo.work@gmail.com>
2022-09-10 21:07:49 +0900

    selectによる停止

--- chap-2/context/main.go
+++ chap-2/context/main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
-
-func child(ctx context.Context) {
-	if err := ctx.Err(); err != nil {
-		return
-	}
-	fmt.Println("キャンセルされていない")
-}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	child(ctx)
+	task := make(chan int)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case i := <-task:
+				fmt.Println("get", i)
+			default:
+				fmt.Println("キャンセルされていない")
+			}
+			time.Sleep(300 * time.Microsecond)
+		}
+	}()
+	time.Sleep(time.Second)
+	for i := 0; 5 > i; i++ {
+		task <- i
+	}
 	cancel()
-	child(ctx)
 }
