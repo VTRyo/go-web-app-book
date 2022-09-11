87b09ac6592611202acd79c88978a2873a16932c
VTRyo <vtryo.work@gmail.com>
2022-09-11 23:01:04 +0900

    context型の値にデータを含める

--- chap-2/context/main.go
+++ chap-2/context/main.go
@@ -3,28 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 )
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	task := make(chan int)
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case i := <-task:
-				fmt.Println("get", i)
-			default:
-				fmt.Println("キャンセルされていない")
-			}
-			time.Sleep(300 * time.Microsecond)
-		}
-	}()
-	time.Sleep(time.Second)
-	for i := 0; 5 > i; i++ {
-		task <- i
+type TraceID string
+
+const ZeroTraceID = ""
+
+type TraceIDKey struct{}
+
+func SetTraceID(ctx context.Context, tid TraceID) context.Context {
+	return context.WithValue(ctx, TraceIDKey{}, tid)
+}
+
+func GetTraceID(ctx context.Context) TraceID {
+	if v, ok := ctx.Value(TraceIDKey{}).(TraceID); ok {
+		return v
 	}
-	cancel()
+	return ZeroTraceID
+}
+
+func main() {
+	ctx := context.Background()
+	fmt.Printf("trace id: %q\n", GetTraceID(ctx))
+	ctx = SetTraceID(ctx, "test-id")
+	fmt.Printf("trace id: %q\n", GetTraceID(ctx))
 }
